2186c737c3aec152a451c7dad3f78bf7a0daecaa
Julio Sandroni <julio.sandroni@gobrax.com>
2024-08-29 14:22:26 -0300

    feat: send streamer

--- cmd/main.go
+++ cmd/main.go
@@ -1,39 +1,46 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"net"
 	"sync"
+
+	"github.com/go-redis/redis/v8"
 )
 
-var wg = sync.WaitGroup{}
+var (
+	wg     = sync.WaitGroup{}
+	rdb    *redis.Client
+	ctx    = context.Background()
+	stream = "paisesStream"
+)
+
+func init() {
+	rdb = redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+}
 
 func send() {
+	defer wg.Done()
+
 	listPaises := []string{"Brasil", "Argentina", "Chile", "Uruguai", "Paraguai", "Bolívia", "Peru", "Colômbia", "Venezuela", "Equador", "Guiana", "Suriname", "Guiana Francesa"}
 
 	for _, item := range listPaises {
-		conn, err := net.Dial("tcp", "localhost:3005")
+		err := rdb.XAdd(ctx, &redis.XAddArgs{
+			Stream: stream,
+			Values: map[string]interface{}{"pais": item},
+		}).Err()
+
 		if err != nil {
+			fmt.Println("Erro ao enviar para o stream:", err)
 			return
 		}
-
-		defer conn.Close()
-
-		_, err = conn.Write([]byte(item))
-
-		if err != nil {
-			fmt.Println(err)
-
-		}
-
 	}
-	wg.Done()
 }
 
 func main() {
 	wg.Add(1)
-	for range 1 {
-		go send()
-	}
+	go send()
 	wg.Wait()
 }
