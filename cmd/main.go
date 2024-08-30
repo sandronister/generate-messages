f9322aeac83026114e7d9d020e49de3b4b764a85
Julio Sandroni <julio.sandroni@gobrax.com>
2024-08-30 14:47:00 -0300

    chore:change main

--- cmd/main.go
+++ cmd/main.go
@@ -11,8 +11,7 @@ import (
 var (
 	wg     = sync.WaitGroup{}
 	rdb    *redis.Client
-	ctx    = context.Background()
-	stream = "paisesStream"
+	stream = "ruptela.com"
 )
 
 func init() {
@@ -24,18 +23,60 @@ func init() {
 func send() {
 	defer wg.Done()
 
-	listPaises := []string{"Brasil", "Argentina", "Chile", "Uruguai", "Paraguai", "Bolívia", "Peru", "Colômbia", "Venezuela", "Equador", "Guiana", "Suriname", "Guiana Francesa"}
+	listPaises := []string{
+		"Brasil",
+		"Argentina",
+		"Chile",
+		"Uruguai",
+		"Paraguai",
+		"Bolívia",
+		"Peru",
+		"Colômbia",
+		"Venezuela",
+		"Equador",
+		"Guiana",
+		"Suriname",
+		"Guiana Francesa",
+		"Estados Unidos",
+		"Canadá",
+		"México",
+		"Cuba",
+		"República Dominicana",
+		"Costa Rica",
+		"Panamá",
+		"China",
+		"Japão",
+		"Coreia do Sul",
+		"Índia",
+		"Rússia",
+		"Alemanha",
+		"França",
+		"Itália",
+		"Espanha",
+		"Portugal",
+	}
 
-	for _, item := range listPaises {
-		err := rdb.XAdd(ctx, &redis.XAddArgs{
-			Stream: stream,
-			Values: map[string]interface{}{"pais": item},
-		}).Err()
+	type message struct {
+		Index int
+		Item  string
+	}
 
+	for i, item := range listPaises {
+		msm := message{
+			Index: i,
+			Item:  item,
+		}
+
+		message := fmt.Sprintf("%v", msm)
+
+		err := rdb.LPush(context.Background(), stream, message).Err()
 		if err != nil {
-			fmt.Println("Erro ao enviar para o stream:", err)
+			fmt.Println("Erro ao adicionar item à fila:", err)
 			return
 		}
+
+		fmt.Println("Item adicionado à fila:", item)
+
 	}
 }
 
